5922afb981ce17c84d0b5a092090eebccd80460c
surajchatterjee0012 <sarojchatterjee2007@gmail.com>
2025-11-12 00:06:51 +0530

    add array2d java program

---
+++ src/Array2D.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Array2D {
+     public static void main(String[] args) {
+         int arr[][] = {{10,20,30},{40,50,60},{70,80,90}};
+
+//         System.out.println(arr[2][2]);
+
+         for (int i = 0; i< arr.length;i++){
+          for (int j = 0; j<arr[i].length;j++){
+              System.out.println(arr[i][j]);
+          }
+             System.out.println("  ");
+         }
+
+
+     }
+}
